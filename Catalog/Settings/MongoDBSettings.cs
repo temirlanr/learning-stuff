c693ca496ec9e8eac4cbec8032cfe52894e6f364
Temirlan Rsanov <86775981+temirlanr@users.noreply.github.com>
2021-09-20 23:48:11 +0600

    Add files via upload

---
+++ Catalog/Settings/MongoDBSettings.cs
@@ -0,0 +1,14 @@
+namespace Catalog.Settings {
+
+    public class MongoDBSettings {
+
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string ConnectionString { 
+            get{
+
+                return $"mongodb:{Host}: {Port}";
+            }
+        }
+    }
+}
