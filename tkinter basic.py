27e3f6b26e048eb300a134d0d5952c43aa2953b7
Temirlan Rsanov <rsanovtemirlan@gmail.com>
2021-09-23 21:17:30 +0600

    starts from basic

---
+++ tkinter basic.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+
+#####
+# myLabel1 = Label(root, text="Hello World!")
+# myLabel2 = Label(root, text="My name is TemirlaN!!!")
+# myLabel3 = Label(root, text="       ")
+
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=5)
+# myLabel3.grid(row=2, column=2)
+#####
+
+myButton = Button(root, text="Click", state=DISABLED)
+myButton.pack()
+
+
+
+root.mainloop()
